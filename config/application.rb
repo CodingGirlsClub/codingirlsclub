46e86eef3b2e8db08b87e839fb97719d51864d19
ruoshuigao <jannegao@gmail.com>
2017-05-07 19:10:00 +0800

    migrate fonts and modify css files

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Codingirlsclub
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile << %w(vendor/assets/*)
+    config.assets.precompile << %w(*.png *.jpg *.jpeg *.gif)
   end
 end
