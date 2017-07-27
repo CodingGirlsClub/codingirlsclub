f57c5512d7acac9c737d1274eccc69a335138682
ruoshuigao <jannegao@gmail.com>
2017-07-27 17:34:42 +0800

    fix supply mentor bug

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,25 +1,5 @@
-# == Schema Information
-#
-# Table name: mentors
-#
-#  id               :integer          not null, primary key
-#  user_id          :integer
-#  introduce_self   :text(65535)
-#  why_to_teach     :text(65535)
-#  master_lang      :string(255)
-#  teaching_time    :string(255)
-#  phone_number     :string(255)
-#  wechat_id        :string(255)
-#  github_url       :string(255)
-#  ever_project_url :string(255)
-#  resume_url       :string(255)
-#  applied          :boolean          default("0")
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#
-
 class Mentor < ApplicationRecord
-  # FIXME: validates :city_id,          presence: true
+  validates :city_id,          presence: true
   validates :user_id,          presence: true
   validates :introduce_self,   presence: true, length: { maximum: 200 }
   validates :course_ids,       presence: true
