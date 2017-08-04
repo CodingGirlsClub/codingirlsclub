9478c4cba80b57672628edc87e5703b3574b1766
ruoshuigao <jannegao@gmail.com>
2017-08-04 14:51:45 +0800

    modify deploy file

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
 server '172.104.78.40', user: 'deploy', roles: %w{web app db}
 
-set :cgc_root_domain, 'codingirls.net'
+set :cgc_root_domain, 'codingirls.club'
 # If use ssl, set second params as true
-set :cgc_sub_domains, [['test.zhubaijia.net', false]]
+set :cgc_sub_domains, [['test.codingirls.club', false]]
 
 set :branch, 'develop'
 set :puma_workers, 2
